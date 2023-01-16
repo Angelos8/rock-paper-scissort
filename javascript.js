498673b5902a38a5139b9e5f6290a3ec0fbfb608
I-N-Genious <a1825116@student.adelaide.edu.au>
2023-01-16 12:17:34 +1030

    functionality finalised

--- javascript.js
+++ javascript.js
@@ -20,36 +20,50 @@ function playRound(playerMove,  computerSelection) {
         gameResultDiv.innerText = `player score = ${playerScore} ---- computer score = ${computerScore}\nIt's a tie!`;
     }
     else if (playerMove == "paper" && computerMove == "rock") {
+        playerScore++;
         console.log("Player wins!");
-        gameResultDiv.innerText = `player score = ${playerScore++} ---- computer score = ${computerScore}\nPlayer wins!`;
+        gameResultDiv.innerText = `player score = ${playerScore} ---- computer score = ${computerScore}\nPlayer wins!`;
     }
     else if (playerMove == "scissors" && computerMove == "rock") {
+        computerScore++;
         console.log("Computer wins!");
-        gameResultDiv.innerText = `player score = ${playerScore} ---- computer score = ${computerScore++}\nComputer wins!`;
+        gameResultDiv.innerText = `player score = ${playerScore} ---- computer score = ${computerScore}\nComputer wins!`;
     }
     else if (playerMove == "rock" && computerMove == "paper") {
+        computerScore++;
         console.log("Computer wins!");
-        gameResultDiv.innerText = `player score = ${playerScore} ---- computer score = ${computerScore++}\nComputer wins!`;
+        gameResultDiv.innerText = `player score = ${playerScore} ---- computer score = ${computerScore}\nComputer wins!`;
     }
     else if (playerMove == "paper" && computerMove == "paper") {
         console.log("It's a tie!");
         gameResultDiv.innerText = `player score = ${playerScore} ---- computer score = ${computerScore}\nIt's a tie!`;
     }
     else if (playerMove == "scissors" && computerMove == "paper") {
+        playerScore++;
         console.log("Player wins!");
-        gameResultDiv.innerText = `player score = ${playerScore++} ---- computer score = ${computerScore}\nPlayer wins!`;
+        gameResultDiv.innerText = `player score = ${playerScore} ---- computer score = ${computerScore}\nPlayer wins!`;
     }
     else if (playerMove == "rock" && computerMove == "scissors") {
+        playerScore++;
         console.log("Player wins!");
-        gameResultDiv.innerText = `player score = ${playerScore++} ---- computer score = ${computerScore}\nPlayer wins!`;
+        gameResultDiv.innerText = `player score = ${playerScore} ---- computer score = ${computerScore}\nPlayer wins!`;
     }
     else if (playerMove == "paper" && computerMove == "scissors") {
+        computerScore++;
         console.log("Computer wins!");
-        gameResultDiv.innerText = `player score = ${playerScore} ---- computer score = ${computerScore++}\nComputer wins!`;
+        gameResultDiv.innerText = `player score = ${playerScore} ---- computer score = ${computerScore}\nComputer wins!`;
     }
     else if (playerMove == "scissors" && computerMove == "scissors") {
         console.log("It's a tie!");
         gameResultDiv.innerText = `player score = ${playerScore} ---- computer score = ${computerScore}\nIt's a tie!`;
+    }
+    if (playerScore == 5 && computerScore < 5){
+        gameResultDiv.innerText = "Player is the winner of the game!";
+        computerScore = 0, playerScore = 0;
+    }
+    else if (computerScore == 5 && playerScore <5){
+        gameResultDiv.innerText = "Computer is the winner of the game!";
+        computerScore = 0, playerScore = 0;
     }
 }
 
